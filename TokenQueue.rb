50c25614609507d2fd435fbaf5725c19db083740
Jared Wheler <jwheelcomp3@gmail.com>
2013-11-09 15:37:21 -0500

    TokenQueue Structure
    This should be the end product of the Tokenizer, then consumed by the
    Parser.  Feel free to change it, as long as the overall logic of it,
    FIFO with progression only when asked for, and the get function results
    are maintained.

---
+++ TokenQueue.rb
@@ -0,0 +1,33 @@
+class TokenQueue
+	#First In, First Out -- Queue
+
+	def initialize
+		@queue = []
+		@position = 0
+	end
+
+  #input expected to be {:content => 'foaf', :type => 'id'} format
+  def add(input)
+		@queue = @queue.push(input)
+  end
+
+  def next
+		@position += 1
+  end
+
+	def get_type
+		@queue[@position][:type] unless @queue[@position] == nil
+	end
+
+	def get_follow_type
+		@queue[@position + 1][:type] unless @queue[@position + 1] == nil
+	end
+
+  def get_content
+	  @queue[@position][:content] unless @queue[@position] == nil
+  end
+
+	def get_follow_content
+		@queue[@position + 1][:content] unless @queue[@position + 1] == nil
+	end
+end
--- readme.txt
+++
@@ -1,5 +0,0 @@
-Programming Language to generate a subset of RDF/XML for Programming Languages Paradigms course at UNF.
-
-Authors:
-Matt Walston
-Jared Wheeler
